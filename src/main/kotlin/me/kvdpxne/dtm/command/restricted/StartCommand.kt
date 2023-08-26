41c0c1f96332904352ce599317042c48399e485f
kvdpxne <kvdpxne@gmail.com>
2023-08-26 23:21:17 +0200

    Improved starting and stopping the game via command.
    From now on, the person who is in the game does not have to enter its name to start or stop it.

--- src/main/kotlin/me/kvdpxne/dtm/command/restricted/StartCommand.kt
+++ src/main/kotlin/me/kvdpxne/dtm/command/restricted/StartCommand.kt
@@ -2,25 +2,34 @@ package me.kvdpxne.dtm.command.restricted
 
 import me.kvdpxne.dtm.command.Executor
 import me.kvdpxne.dtm.command.Parameter
+import me.kvdpxne.dtm.game.Game
 import me.kvdpxne.dtm.game.GameManager
+import me.kvdpxne.dtm.user.User
 import me.kvdpxne.dtm.user.UserPerformer
 
 object StartCommand : Executor<UserPerformer> {
 
+  private fun startGame(game: Game?, user: User) {
+    if (null == game) {
+      user.sendMessage("No found game.")
+      user.sendMessage("Usage: /dtm start <GAME_NAME>")
+      return
+    }
+
+    game.start(user)
+    user.sendMessage("The game ${game.name} has started.")
+  }
+
   override fun execute(performer: UserPerformer, parameter: Parameter) {
     if (parameter.isEmpty()) {
-      performer.sendMessage("Usage: /dtm start <GAME_NAME>")
+      performer.user.run {
+        startGame(GameManager.findByUser(this), this)
+      }
       return
     }
 
-    val gameName = parameter.asText()
-    val game = GameManager.findByName(gameName)
-
-    if (null == game) {
-      performer.sendMessage("No found game.")
-      return
+    performer.user.run {
+      startGame(GameManager.findByName(parameter.asText()), this)
     }
-
-    game.start(performer.user)
   }
 }
--- src/main/kotlin/me/kvdpxne/dtm/command/restricted/StopCommand.kt
+++ src/main/kotlin/me/kvdpxne/dtm/command/restricted/StopCommand.kt
@@ -2,33 +2,43 @@ package me.kvdpxne.dtm.command.restricted
 
 import me.kvdpxne.dtm.command.Executor
 import me.kvdpxne.dtm.command.Parameter
+import me.kvdpxne.dtm.game.Game
 import me.kvdpxne.dtm.game.GameManager
+import me.kvdpxne.dtm.user.User
 import me.kvdpxne.dtm.user.UserPerformer
 
 object StopCommand : Executor<UserPerformer> {
 
-  override fun execute(performer: UserPerformer, parameter: Parameter) {
-    if (parameter.isEmpty()) {
-      performer.sendMessage("Usage: /dtm stop <GAME_NAME>")
-      return
-    }
-
-    val gameName = parameter.asText()
-    val game = GameManager.findByName(gameName)
-
+  private fun stopGame(game: Game?, user: User) {
     if (null == game) {
-      performer.sendMessage("No found game.")
+      user.sendMessage("No found game.")
+      user.sendMessage("Usage: /dtm stop <GAME_NAME>")
       return
     }
 
     try {
-      game.stop(performer.user)
+      game.stop(user)
     } catch (exception: IllegalArgumentException) {
       // It's stupid but for the current phase the point is that the world in
       // which the player is located will be unloaded. it cannot be discharged
       // while any player is on this world.
-      performer.sendMessage("The game cannot be stopped.")
+      user.sendMessage("The game cannot be stopped.")
       return
+    }
+
+    user.sendMessage("The ${game.name} game has been stopped.")
+  }
+
+  override fun execute(performer: UserPerformer, parameter: Parameter) {
+    if (parameter.isEmpty()) {
+      performer.user.run {
+        stopGame(GameManager.findByUser(this), this)
+      }
+      return
+    }
+
+    performer.user.run {
+      stopGame(GameManager.findByName(parameter.asText()), this)
     }
   }
 }
