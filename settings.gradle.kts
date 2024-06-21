ce9d0ad01c9320199fa77a129434907fe085570c
kvdpxne <kvdpxne@gmail.com>
2024-06-21 21:29:17 +0200

    Update settings.gradle.kts: Add global repositories and repository mode

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,17 +2,35 @@ pluginManagement {
 
   repositories {
     gradlePluginPortal()
+
+    mavenCentral()
+    mavenLocal()
   }
 }
 
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
 
   versionCatalogs {
     val fileName = "libraries"
     create(fileName) {
-      from(files("./gradle/${fileName}.versions.toml"))
+      from(files("gradle/$fileName.versions.toml"))
     }
   }
+
+  repositories {
+    mavenCentral()
+    mavenLocal()
+
+    maven {
+      url = uri("https://jitpack.io")
+      content {
+        includeGroupAndSubgroups("com.github.kvdpxne")
+      }
+    }
+  }
+
+  repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
 }
 
 rootProject.name = "destroy-the-monument"
