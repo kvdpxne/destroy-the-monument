8d796160df95052e63dc20d4dcac801a642a69c7
kvdpxne <kvdpxne@gmail.com>
2024-06-22 18:51:03 +0200

    Add a simpler solution for coloring text.

--- src/main/kotlin/me/kvdpxne/dtm/PluginContext.kt
+++ src/main/kotlin/me/kvdpxne/dtm/PluginContext.kt
@@ -1,8 +1,16 @@
 package me.kvdpxne.dtm
 
+import me.kvdpxne.dtm.PluginContext.textFormatter
 import me.kvdpxne.dtm.shared.TextFormatter
 
 object PluginContext {
 
   lateinit var textFormatter: TextFormatter
 }
+
+fun String.colorize(): String = textFormatter.format(this)
+
+fun Array<String>.colorizeAll(): List<String> = map { it.colorize() }
+
+fun <T> T.colorizeAll(): List<String>
+  where T : Iterable<String> = map { it.colorize() }
