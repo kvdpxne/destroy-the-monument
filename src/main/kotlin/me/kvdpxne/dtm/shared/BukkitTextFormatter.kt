8eceecc9102f2c33e2e354b3b8432692a54fb557
kvdpxne <kvdpxne@gmail.com>
2024-06-24 23:40:54 +0200

    Add condition to prevent formatting of empty/blank text.

--- src/main/kotlin/me/kvdpxne/dtm/shared/BukkitTextFormatter.kt
+++ src/main/kotlin/me/kvdpxne/dtm/shared/BukkitTextFormatter.kt
@@ -5,6 +5,9 @@ import org.bukkit.ChatColor
 object BukkitTextFormatter : TextFormatter {
 
   override fun format(text: String): String {
+    if (text.isEmpty() || text.isBlank()) {
+      return text
+    }
     return ChatColor.translateAlternateColorCodes('&', text)
   }
 }
