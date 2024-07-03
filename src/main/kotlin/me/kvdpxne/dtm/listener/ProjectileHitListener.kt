8b88d46c05ab10139d63f31b080722cbe3d7fead
kvdpxne <kvdpxne@gmail.com>
2024-07-03 18:25:05 +0200

    Added ability for archer profession.

---
+++ src/main/kotlin/me/kvdpxne/dtm/listener/ProjectileHitListener.kt
@@ -0,0 +1,71 @@
+package me.kvdpxne.dtm.listener
+
+import me.kvdpxne.dtm.game.GameManager
+import me.kvdpxne.dtm.user.UserManager
+import org.bukkit.entity.Arrow
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.ProjectileHitEvent
+
+object ProjectileHitListener : Listener {
+
+  @EventHandler
+  fun handleProjectileHit(event: ProjectileHitEvent) {
+    val projectile = event.entity
+    if (projectile !is Arrow) {
+      return
+    }
+
+    @Suppress("DEPRECATION")
+    val shooter = projectile.shooter
+    if (shooter !is Player) {
+      return
+    }
+
+    //
+    val user = UserManager.findByIdentifier(shooter.uniqueId) ?: return
+
+    //
+    val game = GameManager.findByUser(user) ?: return
+
+    if (
+      !game.state.isStarted() ||
+      null == game.currentArena ||
+      !game.isInTeam(user) ||
+      !game.isInArenaMap(user)
+    ) {
+      return
+    }
+
+    //
+    val team = game.findTeam(user) ?: return
+
+    val teammate = team.findTeammate(user) ?: return
+
+    // Current profession
+    val profession = teammate.professionQueuingPair.current
+
+    if (profession.name != "archer") {
+      return
+    }
+
+    if (!profession.ability?.ready!!) {
+      return
+    }
+
+    projectile.apply {
+      val location = this.location
+      this.world.createExplosion(
+        location.x,
+        location.y,
+        location.z,
+        3.875F,
+        false,
+        true
+      )
+    }
+
+    profession.ability?.run(shooter)
+  }
+}
