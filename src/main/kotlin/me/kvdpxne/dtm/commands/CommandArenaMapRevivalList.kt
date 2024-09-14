8aca9bf37cc2f30db6a42d7759cef27165336227
kvdpxne <kvdpxne@gmail.com>
2024-09-14 14:05:49 +0200

    Add command for checking revival position of teams.

--- src/main/kotlin/me/kvdpxne/dtm/commands/CommandArenaMapRevivalList.kt
+++ src/main/kotlin/me/kvdpxne/dtm/commands/CommandArenaMapRevivalList.kt
@@ -2,14 +2,40 @@ package me.kvdpxne.dtm.commands
 
 import me.kvdpxne.dtm.command.Command
 import me.kvdpxne.dtm.command.CommandBuilder
+import me.kvdpxne.dtm.command.ParameterBuilder
+import me.kvdpxne.dtm.command.ParameterValidators
 import me.kvdpxne.dtm.command.Performer
+import me.kvdpxne.dtm.game.Arena
+import me.kvdpxne.dtm.game.ArenaService
 
 fun createArenaMapRevivalListCommand(): Command {
   // Usage: /dtm arena map revival list <ARENA_NAME>
   return CommandBuilder()
     .name("list")
+    .parameter(
+      ParameterBuilder<String>()
+        .name("ARENA_NAME")
+        .validationBy(ParameterValidators.STRING_VALIDATOR)
+        .required()
+        .build()
+    )
     .handler<Performer> { performer, arguments ->
+      if (arguments.isEmpty()) {
+        return@handler
+      }
 
+      val arena: Arena? = ArenaService.findArenaByName(arguments.asText())
+      if (null == arena) {
+        return@handler
+      }
+
+      for (it in arena.revivalPositions) {
+        performer.sendMessages(
+          "Team: ${it.team.name}",
+          "x: ${it.x}, y: ${it.y}, z: ${it.z}",
+          "pitch: ${it.pitch}, yaw: ${it.yaw}"
+        )
+      }
     }
     .build()
 }
